a61ed6546b820ddbbd8804088569fcbf141c1777
Matt Willy <matt.willy@gmail.com>
2020-09-23 23:01:04 -0600

    add the code to handle systicks

--- 021task_scheduler/Src/main.c
+++ 021task_scheduler/Src/main.c
@@ -25,15 +25,21 @@ void task2_handler(void);
 void task3_handler(void);
 void task4_handler(void);
 
+void enable_processor_faults(void);
 void init_systick_timer(uint32_t tick_hz);
 void init_scheduler_stack(uint32_t sched_top_of_stack);
 void init_tasks_stack(void);
+void switch_sp_to_psp(void);
 
 
 uint32_t psp_of_tasks[MAX_TASKS] = {T1_STACK_START, T2_STACK_START, T3_STACK_START, T4_STACK_START};
 uint32_t task_handlers[MAX_TASKS];
+int current_task = 0;
 
 int main(void) {
+
+	enable_processor_faults();
+
 	init_scheduler_stack(SCHED_STACK_START);
 
 	task_handlers[0] = (uint32_t)task1_handler;
@@ -44,6 +50,10 @@ int main(void) {
 	init_tasks_stack();
 
 	init_systick_timer(TICK_HZ);
+
+	switch_sp_to_psp();
+
+	task1_handler();
 
 	for(;;);
 }
@@ -120,6 +130,64 @@ void init_tasks_stack(void) {
 	}
 }
 
+void enable_processor_faults(void) {
+	uint32_t *pSHCSR = (uint32_t*)0xE000ED24;
+
+	*pSHCSR |= (1 << 16); // mem manage
+	*pSHCSR |= (1 << 17); // bus fault
+	*pSHCSR |= (1 << 18); // usage fault
+}
+
+uint32_t get_psp_value(void) {
+	return psp_of_tasks[current_task];
+}
+
+__attribute__ ((naked)) void switch_sp_to_psp(void) {
+	//1. initialize the PSP with TASK1 stack start address
+
+	// get the value of psp of current taks
+	__asm volatile("PUSH {LR}"); // preserve LR which connects back to main
+	__asm volatile("BL get_psp_value");
+	__asm volatile("MSR PSP, R0");
+	__asm volatile("POP {LR}");
+
+	//2. change sp to PSP using CONTROL register
+	__asm volatile("MOV R0,#0x02");
+	__asm volatile("MSR CONTROL,R0");
+	__asm volatile("BX LR");
+}
+
+void save_psp_value(uint32_t current_psp_value) {
+	psp_of_tasks[current_task] = current_psp_value;
+}
+
+void update_next_task(void) {
+	current_task++;
+	current_task %= MAX_TASKS;
+}
+
 void SysTick_Handler(void) {
-	printf("Inside systick handler\n");
+	/* Save the context of the current task */
+
+	// !. Get current running task's PSP value
+	__asm volatile("MRS R0,PSP");
+	// 2. Using that PSP value store SF2 (R4 to R11)
+	__asm volatile("STMDB R0!,{R4-R11}");
+
+	// 3. Save the current value of PSP
+	__asm volatile("BL save_psp_value");
+
+	/* Retrieve the current value of the next task */
+
+	// 1. Decide next task to run
+	__asm volatile("BL update_next_task");
+
+	// 2. get its past PSP value
+	__asm volatile("BL get_psp_value");
+
+	// 3. Using that PSP value retrieve SF2(R4 to R11)
+	__asm volatile("LDMIA R0!,{R4-R11}");
+
+	// 4. update PSP and exit
+	__asm volatile("MSR PSP,R0");
 }
