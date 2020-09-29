1d207dabb8456fa3551b95250a14396215fea58f
Matt Willy <matt.willy@gmail.com>
2020-09-28 22:06:30 -0600

    blink 4 leds using tasks

--- 021task_scheduler/Inc/main.h
+++ 021task_scheduler/Inc/main.h
@@ -1,8 +1,7 @@
 #ifndef MAIN_H_
-
 #define MAIN_H_
 
-#define MAX_TASKS 4
+#define MAX_TASKS 5
 
 /* some stack memory calculations */
 #define SIZE_TASK_STACK   1024U
@@ -13,10 +12,12 @@
 #define SRAM_END		  ( (SRAM_START) + (SIZE_SRAM) )
 
 #define T1_STACK_START    SRAM_END
-#define T2_STACK_START    ( (SRAM_END) - (SIZE_TASK_STACK) )
+#define T2_STACK_START    ( (SRAM_END) - (1 * SIZE_TASK_STACK) )
 #define T3_STACK_START    ( (SRAM_END) - (2 * SIZE_TASK_STACK) )
 #define T4_STACK_START    ( (SRAM_END) - (3 * SIZE_TASK_STACK) )
-#define SCHED_STACK_START ( (SRAM_END) - (4 * SIZE_TASK_STACK) )
+#define IDLE_STACK_START  ( (SRAM_END) - (4 * SIZE_TASK_STACK) )
+#define SCHED_STACK_START ( (SRAM_END) - (5 * SIZE_TASK_STACK) )
+
 
 #define TICK_HZ 1000U
 
@@ -25,4 +26,11 @@
 
 #define DUMMY_XPSR 0x01000000U
 
+
+#define TASK_READY_STATE 0x00
+#define TASK_BLOCKED_STATE 0xFF;
+
+#define INTERRUPT_DISABLE() do{__asm volatile ("MOV R0,#0x1"); asm volatile("MSR PRIMASK, R0"); } while(0)
+
+#define INTERRUPT_ENABLE() do{__asm volatile ("MOV R0,#0x0"); asm volatile("MSR PRIMASK, R0"); } while(0)
 #endif
--- 021task_scheduler/Src/main.c
+++ 021task_scheduler/Src/main.c
@@ -18,7 +18,8 @@
  */
 
 #include <stdio.h>
-#include"main.h"
+#include "main.h"
+#include "led.h"
 
 void task1_handler(void);
 void task2_handler(void);
@@ -27,30 +28,31 @@ void task4_handler(void);
 
 void enable_processor_faults(void);
 void init_systick_timer(uint32_t tick_hz);
-void init_scheduler_stack(uint32_t sched_top_of_stack);
+__attribute__ ((naked)) void init_scheduler_stack(uint32_t sched_top_of_stack);
 void init_tasks_stack(void);
 void switch_sp_to_psp(void);
 
+void task_delay(uint32_t tick_count);
 
-uint32_t psp_of_tasks[MAX_TASKS] = {T1_STACK_START, T2_STACK_START, T3_STACK_START, T4_STACK_START};
-uint32_t task_handlers[MAX_TASKS];
-int current_task = 0;
+int current_task = 1; // task1 is running
+uint32_t g_tick_count = 0;
+
+typedef struct {
+	uint32_t psp_value;
+	uint32_t block_count;
+	uint8_t current_state;
+	void (*task_handler)(void);
+}TCB_t;
+
+TCB_t user_tasks[MAX_TASKS];
 
 int main(void) {
 
 	enable_processor_faults();
-
 	init_scheduler_stack(SCHED_STACK_START);
-
-	task_handlers[0] = (uint32_t)task1_handler;
-	task_handlers[1] = (uint32_t)task2_handler;
-	task_handlers[2] = (uint32_t)task3_handler;
-	task_handlers[3] = (uint32_t)task4_handler;
-
 	init_tasks_stack();
-
+	led_init_all();
 	init_systick_timer(TICK_HZ);
-
 	switch_sp_to_psp();
 
 	task1_handler();
@@ -58,27 +60,43 @@ int main(void) {
 	for(;;);
 }
 
+void idle_task() {
+	while(1);
+}
+
 void task1_handler(void) {
 	while(1) {
-		printf("This is task1\n");
+		led_on(LED_GREEN);
+		task_delay(1000);
+		led_off(LED_GREEN);
+		task_delay(1000);
 	}
 }
 
 void task2_handler(void) {
 	while(1) {
-		printf("This is task2\n");
+		led_on(LED_ORANGE);
+		task_delay(500);
+		led_off(LED_ORANGE);
+		task_delay(500);
 	}
 }
 
 void task3_handler(void) {
 	while(1) {
-		printf("This is task3\n");
+		led_on(LED_BLUE);
+		task_delay(250);
+		led_off(LED_BLUE);
+		task_delay(250);
 	}
 }
 
 void task4_handler(void) {
 	while(1) {
-		printf("This is task4\n");
+		led_on(LED_RED);
+		task_delay(125);
+		led_off(LED_RED);
+		task_delay(125);
 	}
 }
 
@@ -106,17 +124,38 @@ __attribute__((naked)) void init_scheduler_stack(uint32_t sched_top_of_stack) {
 	__asm volatile("BX LR");
 }
 
+
+
 void init_tasks_stack(void) {
+	user_tasks[0].current_state = TASK_READY_STATE;
+	user_tasks[1].current_state = TASK_READY_STATE;
+	user_tasks[2].current_state = TASK_READY_STATE;
+	user_tasks[3].current_state = TASK_READY_STATE;
+	user_tasks[4].current_state = TASK_READY_STATE;
+
+	user_tasks[0].psp_value = IDLE_STACK_START;
+	user_tasks[1].psp_value = T1_STACK_START;
+	user_tasks[2].psp_value = T2_STACK_START;
+	user_tasks[3].psp_value = T3_STACK_START;
+	user_tasks[4].psp_value = T4_STACK_START;
+
+	user_tasks[0].task_handler = idle_task;
+	user_tasks[1].task_handler = task1_handler;
+	user_tasks[2].task_handler = task2_handler;
+	user_tasks[3].task_handler = task3_handler;
+	user_tasks[4].task_handler = task4_handler;
+
+
 	uint32_t *pPSP;
 
 	for(int i = 0; i < MAX_TASKS; i++) {
-		pPSP = (uint32_t*) psp_of_tasks[i];
+		pPSP = (uint32_t*) user_tasks[i].psp_value;
 
 		pPSP--;
 		*pPSP = DUMMY_XPSR; // 0x001000000
 
 		pPSP--; // PC
-		*pPSP = task_handlers[i];
+		*pPSP = (uint32_t) user_tasks[i].task_handler;
 
 		pPSP--; // LR
 		*pPSP = 0xFFFFFFFD;
@@ -126,7 +165,7 @@ void init_tasks_stack(void) {
 			*pPSP = 0;
 		}
 
-		psp_of_tasks[i] = (uint32_t)pPSP;
+		user_tasks[i].psp_value = (uint32_t)pPSP;
 	}
 }
 
@@ -139,7 +178,7 @@ void enable_processor_faults(void) {
 }
 
 uint32_t get_psp_value(void) {
-	return psp_of_tasks[current_task];
+	return user_tasks[current_task].psp_value;
 }
 
 __attribute__ ((naked)) void switch_sp_to_psp(void) {
@@ -158,15 +197,46 @@ __attribute__ ((naked)) void switch_sp_to_psp(void) {
 }
 
 void save_psp_value(uint32_t current_psp_value) {
-	psp_of_tasks[current_task] = current_psp_value;
+	user_tasks[current_task].psp_value = current_psp_value;
 }
 
 void update_next_task(void) {
-	current_task++;
-	current_task %= MAX_TASKS;
+	int state = TASK_BLOCKED_STATE;
+
+	for(int i=0; i < MAX_TASKS; i++) {
+		current_task++;
+		current_task %= MAX_TASKS;
+		state = user_tasks[current_task].current_state;
+		if( (state == TASK_READY_STATE) && (current_task != 0) )
+			break;
+	}
+
+	if(state != TASK_READY_STATE)
+		current_task = 0;
+
 }
 
-void SysTick_Handler(void) {
+void schedule(void) {
+	// pend the pendsv exception
+	uint32_t *pICSR = (uint32_t*)0xE000ED04;
+	*pICSR |= (1 << 28);
+}
+
+void task_delay(uint32_t tick_count) {
+
+	// disable interrupt
+	INTERRUPT_DISABLE();
+
+	if(current_task) {
+		user_tasks[current_task].block_count = g_tick_count + tick_count;
+		user_tasks[current_task].current_state = TASK_BLOCKED_STATE;
+		schedule();
+	}
+
+	INTERRUPT_ENABLE();
+}
+
+__attribute__ ((naked)) void PendSV_Handler(void) {
 	/* Save the context of the current task */
 
 	// !. Get current running task's PSP value
@@ -198,6 +268,37 @@ void SysTick_Handler(void) {
 	__asm volatile("BX LR");
 }
 
+void update_global_tick_count(void) {
+	g_tick_count++;
+}
+
+void unblock_tasks(void) {
+	for(int i = 1; i < MAX_TASKS; i++) {
+		if(user_tasks[i].current_state != TASK_READY_STATE) {
+			if(user_tasks[i].block_count == g_tick_count) {
+				user_tasks[i].current_state = TASK_READY_STATE;
+			}
+		}
+	}
+}
+
+void SysTick_Handler(void) {
+	update_global_tick_count();
+	unblock_tasks();
+	schedule();
+}
+
 void HardFault_Handler(void) {
-	printf("Received a hard fault\n");
+	printf("Exception : HArdfault\n");
+	while(1);
+}
+
+void MemManage_Handler(void) {
+	printf("Exception: MemManage\n");
+	while(1);
+}
+
+void BusFault_HAndler(void) {
+	printf("Exception: BusFAult\n");
+	while(1);
 }
